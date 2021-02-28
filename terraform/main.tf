f4c7f6173a63ba49ce66756106faf5f574ffa95f
Alan Wessman <alanyst@gmail.com>
2021-02-27 22:47:12 -0700

    Schema tweaks; add cloudwatch to terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = ">= 3.30.0"
     }
   }
 }
@@ -24,7 +24,55 @@ data "local_file" "openapi_spec" {
   filename = "${path.module}/../schema/edm-api.yml"
 }
 
-resource "aws_apigatewayv2_api" "versa_edm_api_gateway" {
+resource "aws_iam_role" "cloudwatch_role" {
+  name = "cloudwatch_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "apigateway.amazonaws.com"
+        }
+      },
+    ]
+  })
+  inline_policy {
+    name = "cloudwatch_role_policy"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Effect = "Allow"
+          Action = [
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:DescribeLogGroups",
+            "logs:DescribeLogStreams",
+            "logs:PutLogEvents",
+            "logs:GetLogEvents",
+            "logs:FilterLogEvents"
+          ]
+          Resource = "*"
+        }
+      ]
+    })
+  }
+}
+
+resource "aws_cloudwatch_log_group" "api_gateway_log_group_dev" {
+  name              = "api_gateway_log_group_dev"
+  retention_in_days = 180
+}
+
+resource "aws_api_gateway_account" "api_gateway_account" {
+  cloudwatch_role_arn = aws_iam_role.cloudwatch_role.arn
+}
+
+resource "aws_apigatewayv2_api" "api_gateway" {
   name          = local.schema_name
   description   = local.schema_description
   protocol_type = "HTTP"
@@ -33,12 +81,16 @@ resource "aws_apigatewayv2_api" "versa_edm_api_gateway" {
 }
 
 resource "aws_apigatewayv2_stage" "dev" {
-  api_id        = aws_apigatewayv2_api.versa_edm_api_gateway.id
+  api_id        = aws_apigatewayv2_api.api_gateway.id
   name          = "dev"
   auto_deploy   = false
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api_gateway_log_group_dev.arn
+    format = <<-EOT
+      { "requestId":"$context.requestId", "ip": "$context.identity.sourceIp", "requestTime":"$context.requestTime", "httpMethod":"$context.httpMethod","routeKey":"$context.routeKey", "status":"$context.status","protocol":"$context.protocol", "responseLength":"$context.responseLength" }
+    EOT
+  }
 }
-
-# TODO: Add CloudWatch log resources
 
 output "schema_version" {
   value = local.schema_version
